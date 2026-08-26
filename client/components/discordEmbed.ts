9976b14f051f28e4cc531f16ff18b582609ba9b5
Eva <me@eva.pink>
2026-08-26 00:33:24 -0500

    feat: discord-accurate embed preview

--- client/components/discordEmbed.ts
+++ client/components/discordEmbed.ts
@@ -2,42 +2,44 @@ import '../styles/discordEmbed.css'
 import cover from '../assets/images/starfall.webp'
 import profile from '../assets/images/profile.webp'
 
+function message(link: string, embed = '') {
+  return `
+    <section class="discord-chat">
+      <article class="message">
+        <img class="avatar" src="${profile}" alt="" />
+        <div class="body">
+          <header>
+            <span class="username">max</span>
+            <time>Today at 7:21 PM</time>
+          </header>
+          <p class="text"><span class="link">${link}</span></p>
+          ${embed}
+        </div>
+      </article>
+    </section>
+  `;
+}
+
+const richEmbed = `
+  <div class="rich-embed">
+    <div class="grid">
+      <span class="provider">FixSpotify</span>
+      <span class="title">Starfall</span>
+      <p class="description">By SALEM • 2:48
+Track 5 of 11 on Fires in Heaven
+Released October 30, 2020</p>
+      <img class="thumbnail" src="${cover}" alt="Cover of Fires in Heaven by SALEM" />
+    </div>
+  </div>
+`;
+
 export function initDiscordEmbed() {
   const discordEmbed = `
     <section class="discord-embed">
       <h2>Before</h2>
-      <section class="embed">
-        <img class="profile-picture" src=${profile}></img>
-        <section class="content">
-          <section class="infos">
-            <span class="username">max</span>
-            <span>Today at 7:21 PM</span>
-          </section>
-          <p>https://open.spotify.com/track/05FpQ41MVtDd1Ft63DZNuv</p>
-        </section>
-      </section>
+      ${message('https://open.spotify.com/track/05FpQ41MVtDd1Ft63DZNuv')}
       <h2>After</h2>
-      <section class="embed">
-          <img class="profile-picture" src=${profile}></img>
-        <section class="content">
-          <section class="infos">
-            <span class="username">max</span>
-            <span>Today at 7:21 PM</span>
-          </section>
-          <p>https://open.fixspotify.com/track/05FpQ41MVtDd1Ft63DZNuv</p>
-          <section class="fixspotify-embed">
-            <section class="infos">
-              <span>FixSpotify</span>
-              <span class="track-name">Starfall</span>
-              <section class="details">
-                <span>By SALEM • 2:48</span>
-                <span>Track 5 of 11 on Fires in Heaven</span>
-                <span>Released October 30, 2020</span>
-              </section>
-            </section>
-            <img src="${cover}" alt="Cover of Fires in Heaven by SALEM" />
-        </section>
-      </section>
+      ${message('https://open.fixspotify.com/track/05FpQ41MVtDd1Ft63DZNuv', richEmbed)}
     </section>
   `;
   const discordEmbedContainer = document.getElementById('discord-embed-container');
